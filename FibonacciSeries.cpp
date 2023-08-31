f82355ff79ce97a2aba7246a0bc1efafc6f176c5
kunal <kunalkarne2020@gmail.com>
2023-08-31 16:21:07 +0530

    cpp programs

---
+++ FibonacciSeries.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int a = 0 , b = 1 , c , i , number ;
+
+    
+    cout<<"Enter a number " <<endl;
+    cin >> number;
+    cout<< a << " " << b << " " ;
+    for (int i = 2; i <= number; i++)
+    {
+        c = a + b;
+        cout<< c << " " ;
+        a = b;
+        b = c;
+    }
+    
+
+    return 0;
+}
